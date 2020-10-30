5535f3a352e7ccf165ba35ee7b2f4f0dd0682dce
Raina86 <you@example.com>
2020-10-30 12:41:48 -0400

    pushing add button, Bookcard, DefaultBooklist

--- src/components/DefaultBookList.jsx
+++ src/components/DefaultBookList.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import BookCard from './BookCard'
 
 const DefaultBookList = (props) => {
-  return <div></div>;
-};
+  return (
+        <div className ="list">
+         {
+             props.books.map(( book,i) =>{
+             return<BookCard
+                    key={i}
+                    image={book.volumeInfo.imageLinks.thumbnail}
+                    title={book.volumeInfo.title}
+                    author={book.volumeInfo.authors}
+                    published={book.volumeInfo.publishedDate}
+                />
+      })
+    }
+  </div>
+  )
+}
 
 export default DefaultBookList;
