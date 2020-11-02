aae0cb1a5655a4ffb09284787d034e3ddfdc6514
Raina86 <you@example.com>
2020-11-01 21:56:45 -0500

    find better way to add and remove books, and logout. Pushing work on add , delete and logout components

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,103 +1,45 @@
-import React from "react";
-import { Container } from "reactstrap";
-import BookCard from "./BookCard";
-import axios from 'axios';
+import React from 'react';
 
+const App =() => {
+     const [bookvalue, setBookValue] = React.useState('');
+     const[book,setBooks]= React.useState([]);
+     const handleAddTodo =() => {
+          if(bookvalue===''){
+               alert('Please insert the value');
+          }else{
+               const filterValue = book.find(b => === bookvalue)
+               if(filterValue !== undefined){}
+               
+          } else {
+               const defaultBooklist= [... book, bookvalue];
+               setBooks(defaultBooklist);
+            
+          }
+     }
 
-class cart extends component{
+     const removeBook =(index)=>{
+          const remaingBook = book.filter(b => book.indexOf(b)!==);
+          setBooks(remainingBook);
 
+     }
+     
+     return(
+     <div>
+          <div className="defaultbooklist">
+               <h3>Your wishlist</h3>
+               <input type ="text" onChange={(e) => setBookValue(e.target.value)} placeholder="search"/>
+               <input onClick={handleAddBook} type="submit" value="add book"/>
 
+          {
+               book.map(b =><tr><li>{b}</li>
+               <span onClick={() =>
+               removeBook(book.indexof(b))} className="close-symbole">x</span></tr>
+               )
+          }     
 
-     // class AddBooks extends component{
-//     addBooks(newBooks){
-//     axios.request({
-//         method:'post',
-//         url:'',
-//         data: newBooks
-//     }). then(response => {
-//       this.props.history.push('/'); 
-//     }).catch(err=> console.log(err));
-//     }
-    
-//     onSubmit(e){
-//         const newBook ={
-//             title:this.refs.name.value,
-//             author:this.refs.name.value,
-//             publishedDate:this.refs.name.value,
-//         }
-//         this.addBooks(newBooks);
-//         e.preventDefault();
-//     }
-//     render(){
-//   return (
-//       <div> 
-//           <Link className="" to=""></Link>
-//           <h1>Add Books</h1>
-//           <form onSubmit={this.onSubmit.bind(this)}>
-//               <div className="input-field">
-//                   <input type="text" name="title" ref="title"/>
-//                   <label htmlFor="name">Title</label>
-//               </div>
-//               <div className="input-field">
-//                   <input type="text" name="author" ref="author"/>
-//                   <label htmlFor="name">Author</label>
-//                   </div>
-//                   <div className="input-field">
-//                   <input type="text" name="published.Date" ref="publishedDate"/>
-//                   <label htmlFor="name">publishedDate</label>
-//                   </div>
-//               <input type="submit" value="save" className="btn"></input>
-//           </form>
-//       </div>
-//   )
-// }
-   
-// }
-// export default AddBooks;
+          </div>
+     </div>
+     );
+};
 
-    render() {
-        return(
-            <section>
-                <productconsumer>
-                 
-                 {value => {
-                   if (value.cart.length > 0 ) {
-                     return(
-                        <div>
-                          <div><h1> your cart</h1></div>
-                          <div className="container-fluid text -center">
-                            <div className="row">
-                            <div className="col-10 col-lg">
-                                 <strong> Title</strong>
-                            </div>
-                            <div className="col-10 col-lg">
-                                 <strong>Author </strong>
-                            </div>
-                            <div className="col-10 col-lg">
-                                 <strong>PublishedDate</strong>
-                            </div>
-                            </div>
-                            <div className="col-10 col-lg">
-                                 <strong>Remove</strong>
-                            </div>
-                           </div>
-                        </div>
-                      {value.cart.map(cartData =>{
-                           return(
-
-                           )
-                      })}
-                     </div>    
-                     )
-                   }
-                 }}
-           
-                </productconsumer>
-            </section>
-        )
-
-    }
-}
-
-
-export default Cart;
+export default App;
---
+++ src/components/logout.jsx
@@ -0,0 +1,64 @@
+import { render } from "@testing-library/react";
+
+export default class App extends component{
+    constructor() {
+        super();
+
+        this.State = {
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {}
+        };
+
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLogout = this.handlelogout
+    }
+    
+    checkLoginStatus(){
+        axios
+        .get("http://localhost:3001/logged_in" , {withCredentials:true})
+        .then(response => {
+            if(
+                response.data.logged_in &&
+                this.State.loggedInStatus = "NOT_LOGGED_IN"
+            ) {
+              this.setState({
+                  loggedInStatus: "LOGGED_IN"
+                  user:response.data.user
+
+              });
+
+            } else if(
+                !response.data.logged_in &
+                (this.State.loggedInStatus= "LOGGED_IN")
+            ) {
+              this .setState({
+                  loggedInStatus :"NOT_LOGGED_IN",
+                  user:{}
+              });
+        })
+    }
+})
+.catch(error =>{
+    console.log("ckeck login error", error);
+});
+}
+
+componentDidMount =>{
+    this.checkLoginStatus();
+}
+
+handleLogout()
+ this.setState({
+     loggedInStatus: "NOT_LOOGED_IN",
+     user:{}
+ })
+}
+handleLogin(data) {
+    this.setState( {
+        loggedInStatus: "LOGGED_IN",
+        user: data.user
+    });
+}
+render(){
+    
+}
