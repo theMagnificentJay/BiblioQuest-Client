bd590f03990f35efd449057e559d0a6ff4cd6eda
theMagnificentJay <jreed.408@gmail.com>
2020-11-03 15:05:11 -0600

    Display Books working, needs formatting, and not to grow past its container.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,10 +16,7 @@ import Footer from "./Footer";
 import { Container, Row, Col } from "reactstrap";
 
 function Layout() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [results, showResults] = useState([]);
-
-  const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
 
   function searchAll() {
     const searchInput = document.getElementById("searchInput").value;
@@ -34,177 +31,17 @@ function Layout() {
         showResults(res.items);
       });
   }
-  console.log(
-    results.length > 0 ? results[0].volumeInfo.imageLinks.thumbnail : null
-  );
+  console.log(results);
 
-  function searchAuthor() {
-    const searchInput = document.getElementById("searchInput").value;
-    const searchAuthor = "inauthor:";
-
+  function filterBook(filter) {
+    console.log(filter);
     fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=" +
-        searchAuthor +
-        searchInput +
-        "&maxResults=40&printType=books"
+      `https://www.googleapis.com/books/v1/volumes?q=subject:${filter}&maxResults=40&printType=books`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-
-  function searchGenre() {
-    const searchInput = document.getElementById("searchInput").value;
-    const searchGenre = "subject:";
-
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=" +
-        searchGenre +
-        searchInput +
-        "&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-
-  function searchTitle() {
-    const searchInput = document.getElementById("searchInput").value;
-    const searchTitle = "intitle:";
-
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=" +
-        searchTitle +
-        searchInput +
-        "&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-
-  function searchPublisher() {
-    const searchInput = document.getElementById("searchInput").value;
-    const searchPublisher = "inPublisher:";
-
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=" +
-        searchPublisher +
-        searchInput +
-        "&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-
-  function filterFictionYoungAdult() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Young+Adult&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFictionFantasy() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Fantasy&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFictionChildren() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Children&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFictionScifi() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Science+Fiction&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFictionThriller() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Thriller&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFictionHorror() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Horror&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFictionRomance() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Romance&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionEducational() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Educational&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionHistorical() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Historical&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionSelfHelp() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Self+Help&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionReligion() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Religion&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionBiography() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Biography&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionHistory() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:History&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFictionHealth() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Health+and+Fitness&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterFiction() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Fiction&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
-  }
-  function filterNonFiction() {
-    fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=subject:Non-Fiction&maxResults=40&printType=books"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data.items));
+      .then((res) => {
+        showResults(res.items);
+      });
   }
 
   return (
@@ -225,42 +62,9 @@ function Layout() {
                 id="searchInput"
                 placeholder="Search author, genre, title, or publisher . . ."
               />
-              <InputGroupAddon addonType="prepend"><Button onClick={(e) => searchAll()}>Search</Button></InputGroupAddon>
-                  {/* <DropdownToggle caret>Search</DropdownToggle>
-                   <DropdownMenu>
-                    <DropdownItem
-                      className="searchAll"
-                      onClick={(e) => searchAll()}
-                    >
-                      All
-                    </DropdownItem>
-                    <DropdownItem
-                      className="searchAuthor"
-                      onClick={(e) => searchAuthor()}
-                    >
-                      Author
-                    </DropdownItem>
-                    <DropdownItem
-                      className="searchGenre"
-                      onClick={(e) => searchGenre()}
-                    >
-                      Genre
-                    </DropdownItem>
-                    <DropdownItem
-                      className="searchTitle"
-                      onClick={(e) => searchTitle()}
-                    >
-                      Title
-                    </DropdownItem>
-                    <DropdownItem
-                      className="searchPublisher"
-                      onClick={(e) => searchPublisher()}
-                    >
-                      Publisher
-                    </DropdownItem>
-                  </DropdownMenu>
-                  </InputGroupButtonDropdown> 
-              </InputGroupAddon> */}
+              <InputGroupAddon addonType="append">
+                <Button onClick={(e) => searchAll()}>Search</Button>
+              </InputGroupAddon>
             </InputGroup>
           </Col>
           <Col className="colThree col rb" xs="2">
@@ -282,38 +86,51 @@ function Layout() {
                 <p>Staff List</p>
                 <p>Popular</p>
                 <br />
-                <h5 onClick={(e) => filterFiction()}>Fiction</h5>
+                <h5 onClick={(e) => filterBook("fiction")}>Fiction</h5>
                 <hr />
-                <p onClick={(e) => filterFictionYoungAdult()}>Young Adult</p>
-                <p onClick={(e) => filterFictionFantasy()}>Fantasy</p>
-                <p onClick={(e) => filterFictionChildren()}>Children’s</p>
-                <p onClick={(e) => filterFictionScifi()}>Science Fiction</p>
-                <p onClick={(e) => filterFictionThriller}>Thrillers/Suspense</p>
-                <p onClick={(e) => filterFictionHorror}>Horror</p>
-                <p onClick={(e) => filterFictionRomance()}>Romance</p>
-                <br />
-                <h5 onClick={(e) => filterNonFiction()}>Non-Fiction</h5>
-                <hr />
-                <p onClick={(e) => filterNonFictionEducational()}>
-                  Educational
+                <p onClick={(e) => filterBook("young+adult")}>Young Adult</p>
+                <p onClick={(e) => filterBook("fantasy")}>Fantasy</p>
+                <p onClick={(e) => filterBook("children's")}>Children’s</p>
+                <p onClick={(e) => filterBook("science+fiction")}>
+                  Science Fiction
                 </p>
-                <p onClick={(e) => filterNonFictionHistorical()}>Historical</p>
-                <p onClick={(e) => filterNonFictionSelfHelp()}>Self-help</p>
-                <p onClick={(e) => filterNonFictionReligion()}>
+                <p onClick={(e) => filterBook("thriller")}>
+                  Thrillers/Suspense
+                </p>
+                <p onClick={(e) => filterBook("horror")}>Horror</p>
+                <p onClick={(e) => filterBook("romance")}>Romance</p>
+                <br />
+                <h5 onClick={(e) => filterBook("nonfiction")}>Non-Fiction</h5>
+                <hr />
+                <p onClick={(e) => filterBook("educational")}>Educational</p>
+                <p onClick={(e) => filterBook("historical")}>Historical</p>
+                <p onClick={(e) => filterBook("self+help")}>Self-help</p>
+                <p onClick={(e) => filterBook("religion")}>
                   Religion/Spirituality
                 </p>
-                <p onClick={(e) => filterNonFictionBiography()}>Biography</p>
-                <p onClick={(e) => filterNonFictionHistory()}>History</p>
-                <p onClick={(e) => filterNonFictionHealth()}>
-                  Health and Fitness
-                </p>
+                <p onClick={(e) => filterBook("biography")}>Biography</p>
+                <p onClick={(e) => filterBook("history")}>History</p>
+                <p onClick={(e) => filterBook("health")}>Health and Fitness</p>
               </div>
             </div>
           </Col>
           <Col className="colTwo brownBG rb">
             <Row className="popBookCarousel rb">
-              <Col id="displaySearchContent" className="colColor rb bsb">
-                {results.length > 0 ? ( <img src={results[0].volumeInfo.imageLinks.thumbnail} alt="" />) : null}
+              <Col id="displaySearchContent" className="displayBooks colColor rb bsb">
+                {results.length > 0 ? (
+                  results.map((bookItem) => {
+                    return (
+                      <div className="card-container">
+                        <img
+                          src={bookItem.volumeInfo.imageLinks.thumbnail}
+                          alt=""
+                        />
+                      </div>
+                    );
+                  })
+                ) : (
+                  <p>empty</p>
+                )}
               </Col>
             </Row>
           </Col>
